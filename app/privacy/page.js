60ad194803da7e712162f9ff1ddedfa6a56df982
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-09-06 18:30:15 +0530

    added the terms and conditions pages as google cloud console asked for it

---
+++ app/privacy/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Privacy() {
+    return (
+        <div className="">
+            your privacy is our responsibility.<br />
+            we believe in delivering the user with the best experience by using the user privacy centric approach.<br />
+            in this approach we, are open to our users about the information we take from them, all that information is boound to be displayed on the user profile screen, and is accessible to no one other than us to provide our user with utmost experience of our site.
+        </div>
+    )
+}
---
+++ app/terms-and-conditions/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function TermsAndConditions() {
+    return (
+        <div className="">
+            Terms:<br/>
+            We will be collecting your :<br/>
+            1. name<br/>
+            2. email<br/>
+            3. photo <br/>
+            These will be collected just to know who we are messaging to.<br/>
+            Just to keep transarency between the user and the developer of this website.
+            <br/>
+            Conditions:<br/>
+            We request you to please make sure you maintain your privacy at your end also, although we will provide you with privacy.<br/>
+            Never provide any falsy information to us.<br/><br/>
+            We are at your service, if you face any issue. Enjoy our app.
+        </div>
+    )
+}
