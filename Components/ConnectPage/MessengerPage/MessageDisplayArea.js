14acb646992638909cb54e12b6a415b3fe2ccd17
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-11-27 16:33:05 +0530

    aome ui changes on connect messenger

--- Components/ConnectPage/MessengerPage/MessageDisplayArea.js
+++ Components/ConnectPage/MessengerPage/MessageDisplayArea.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function MessageDisplayArea() {
     return (
-        <div className="w-full min-h-[70vh] h-full">
+        <div className="w-full md:w-[65%] lg:w-[60%] min-h-[60vh] md:min-h-[70vh] h-full">
         {/* This is the area where we will display the messages */}
         </div>
     )
