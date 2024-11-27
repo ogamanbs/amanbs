9328e7bb63453a0c4df9500859a9ede7ce3dd0d7
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-11-27 21:21:49 +0530

    added the leetcode link in the navbar and footer

--- Components/LandingPage/Footer/SocialLinks.js
+++ Components/LandingPage/Footer/SocialLinks.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import leetcode from '../../../public/leetcode.png';
 import {
     RiInstagramLine,
     RiGithubFill,
@@ -7,6 +8,7 @@ import {
     RiGlobalLine,
     RiMailLine,
 } from '@remixicon/react';
+import Image from 'next/image';
 
 const SocialLinks = () => {
     return (
@@ -14,7 +16,11 @@ const SocialLinks = () => {
             <Link href="https://www.instagram.com/itsogaman/" passHref legacyBehavior><a target="_blank"><RiInstagramLine size={35} color="white" className="cursor-pointer hover:scale-125" /></a></Link>
             <Link href="https://github.com/ogamanbs" passHref legacyBehavior><a target="_blank"><RiGithubFill size={35} color="white" className="cursor-pointer hover:scale-125" /></a></Link>
             <Link href="https://www.linkedin.com/in/aman-sharma-233450226" passHref legacyBehavior><a target="_blank"><RiLinkedinFill size={35} color="white" className="cursor-pointer hover:scale-125" /></a></Link>
-            <Link href="https://www.amanbs.com/"><RiGlobalLine size={35} color="white" className="cursor-pointer hover:scale-125" /></Link>
+            <Link href="https://leetcode.com/u/amansharma_se/">
+                <div className="h-[30px] w-[30px] overflow-hidden">
+                    <Image className="h-full w-full object-contain" src={leetcode} alt={"leetcode"} width={1440} height={1440} />
+                </div>
+            </Link>
             <Link href="mailto:amansharmase100@gmail.com"><RiMailLine size={35} color="white" className="cursor-pointer hover:scale-125" /></Link>
         </div>
     )
