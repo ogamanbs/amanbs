5a8047e7f86266a78dc3a0a0d5648bcc726ad069
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2025-05-09 07:28:43 +0530

    changed the name of the poject

--- Components/LandingPage/Footer/ProjectLinks.js
+++ Components/LandingPage/Footer/ProjectLinks.js
@@ -7,7 +7,7 @@ const ProjectLinks = () => {
             <h1 className="text-xl font-bold">Projects</h1>
                 <div className="flex flex-col gap-1 text-sm items-center ">
                     <Link href="https://bitem.in"><div className="rounded-full hover:scale-110 hover:text-center hover:font-bold hover:cursor-pointer">B I T E M</div></Link>
-                    <Link href="https://strokeml.vercel.app"><div className="rounded-full hover:scale-110 hover:text-center hover:font-bold hover:cursor-pointer">S T R O K E &nbsp; A P P</div></Link>
+                    <Link href="https://strokeml.vercel.app"><div className="rounded-full hover:scale-110 hover:text-center hover:font-bold hover:cursor-pointer">H E A R T &nbsp; S T R O K E &nbsp; A P P</div></Link>
                 </div>
         </div>
     );
--- Components/LandingPage/projects/Strokeml.js
+++ Components/LandingPage/projects/Strokeml.js
@@ -18,12 +18,12 @@ export default function Strokeml() {
                 </div>
             </div>
             <div className="h-auto md:h-full w-full md:w-[50%] px-5 flex flex-col justify-center">
-                <h1 className={`text-6xl md:text-9xl ${bebas_Neue.className} text-red-600`}>Stroke Prediction App</h1>
+                <h1 className={`text-6xl md:text-9xl ${bebas_Neue.className} text-red-600`}>Heart Stroke Prediction App</h1>
                 <div className="block md:hidden h-full w-full border border-zinc-800 rounded-[20px] overflow-hidden bg-white/10 mb-5 mt-3">
                     <Image className="h-full w-full object-contain" src={'/strokeml.png'} alt={"Stroke Prediction App"} width={1440} height={1440} />
                 </div>
                 <div className="">
-                    This is a Stroke prediction app which is developed using flask and react where the dataset to train the model was picked up from kaggle.
+                    This is a Heart Stroke prediction app which is developed using flask and react where the dataset to train the model was picked up from kaggle.
                     In this project I have used various machine learning concepts like linear regression and pipelining etc.
                     In this project if you input the asked fields like age, gender, bmi etc., then it will display a message that if you are likely to get a stroke and you need to get diagnosed or not.
                 </div>
